0fbb1f2a6014506b7fd969c3f5bd38dc13b96e87
EthioSpiderTeam <32637179+EthioSpiderTeam@users.noreply.github.com>
2023-02-06 03:42:18 +0300

    1 binaries uploaded

---
+++ app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <title>Mums for Mums</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import "../styles/globals.css"
+
+export default function HomePage() {
+  return (
+    <div>
+      <h1 className="text-4xl">Welcome to mums for mums blogMums</h1>
+    </div>
+  );
+}
